ccea88a8723f77283f403e511047b59f9f55a6bf
Gleb Laychenkov <racers2no95@mail.ru>
2024-05-19 12:18:58 +0400

    commit_8

--- src/test/java/ru/netology/test/CardDeliveryDateTest.java
+++ src/test/java/ru/netology/test/CardDeliveryDateTest.java
@@ -1,9 +1,10 @@
-package ru.netology;
+package ru.netology.test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import ru.netology.data.DataGenerator;
 
 import java.time.Duration;
 
